6c81c256760e8aaae7f0f38a4433b94eb56850cd
Tjitse Efdé <tjitse@vendic.nl>
2024-08-19 13:21:02 +0200

    Fix for disappearing checkbox

--- Magewire/SubscribeInfo.php
+++ Magewire/SubscribeInfo.php
@@ -8,6 +8,7 @@ namespace Vendic\HyvaCheckoutNewsletterSubscribe\Magewire;
 use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\Newsletter\Model\GuestSubscriptionChecker;
 use Magewirephp\Magewire\Component;
+use Vendic\HyvaCheckoutNewsletterSubscribe\Service\NewsletterSubscriptionChecker;
 
 class SubscribeInfo extends Component
 {
@@ -16,7 +17,12 @@ class SubscribeInfo extends Component
     /**
      * @var bool
      */
-    public $hasSubscription = false;
+    public $checked = false;
+
+    /**
+     * @var bool
+     */
+    public $hidden = false;
 
     /**
      * @var array
@@ -25,23 +31,35 @@ class SubscribeInfo extends Component
 
     public function __construct(
         private CheckoutSession $checkoutSession,
-        private GuestSubscriptionChecker $guestSubscriptionChecker
+        private NewsletterSubscriptionChecker $newsletterSubscriptionChecker
     ) {
     }
 
     public function mount(): void
     {
-        $this->hasSubscription = $this->checkoutSession->getData(self::HAS_SUBSCRIPTION) ?? false;
+        // Hide for logged in customers that are already subscribed
+        if ($this->checkoutSession->getQuote()->getCustomer()->getId()) {
+            $email = $this->checkoutSession->getQuote()->getCustomer()->getEmail();
+            $this->hidden = $this->newsletterSubscriptionChecker->isSubscribed($email);
+        }
+
+        // Scenario for users that have previously entered the checkout
+        if ($this->checkoutSession->getData(self::HAS_SUBSCRIPTION)) {
+            $this->checked = $this->checkoutSession->getData(self::HAS_SUBSCRIPTION) ?? false;
+        }
     }
 
     public function hideIfHasSubscription(?string $email): void
     {
         if (!$email) {
+            $this->hidden = false;
             return;
         }
 
-        $value = $this->guestSubscriptionChecker->isSubscribed($email);
-        $this->checkoutSession->setData(self::HAS_SUBSCRIPTION, $value);
-        $this->hasSubscription = $value;
+        if (!$this->newsletterSubscriptionChecker->isSubscribed($email)) {
+            $this->hidden = false;
+            return;
+        }
+        $this->hidden = true;
     }
 }
---
+++ Service/NewsletterSubscriptionChecker.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace  Vendic\HyvaCheckoutNewsletterSubscribe\Service;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Newsletter\Model\Subscriber;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * @copyright   Copyright (c) Vendic B.V https://vendic.nl/
+ */
+class NewsletterSubscriptionChecker
+{
+    public function __construct(
+        private ResourceConnection $resourceConnection,
+        private StoreManagerInterface $storeManager
+    ) {
+    }
+
+    /**
+     * Check is subscribed by email
+     */
+    public function isSubscribed(string $subscriberEmail): bool
+    {
+        if (!empty($subscriberEmail)) {
+            $storeIds = $this->storeManager->getWebsite()->getStoreIds();
+            $connection = $this->resourceConnection->getConnection();
+            $select = $connection
+                ->select()
+                ->from($this->resourceConnection->getTableName('newsletter_subscriber'))
+                ->where('subscriber_email = ?', $subscriberEmail)
+                ->where('subscriber_status = ?', Subscriber::STATUS_SUBSCRIBED)
+                ->where('store_id IN (?)', $storeIds)
+                ->limit(1);
+
+            return (bool)$connection->fetchOne($select);
+        }
+
+        return false;
+    }
+}
