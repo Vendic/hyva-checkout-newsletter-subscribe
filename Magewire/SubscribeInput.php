94b0f7ca813f0ce222ce8d9cb47cf10618ea315a
Andrei Lisouski <andrei@vendic.nl>
2025-09-24 16:46:33 +0200

    Add configuration for checkbox initial state

--- Magewire/SubscribeInput.php
+++ Magewire/SubscribeInput.php
@@ -1,29 +1,36 @@
-<?php declare(strict_types=1);
+<?php
+
 /**
  * @copyright   Copyright (c) Vendic B.V https://vendic.nl/
  */
+
+declare(strict_types=1);
 
 namespace Vendic\HyvaCheckoutNewsletterSubscribe\Magewire;
 
 use Magento\Checkout\Model\Session as CheckoutSession;
 use Magewirephp\Magewire\Component;
+use Vendic\HyvaCheckoutNewsletterSubscribe\Model\Config;
 
 class SubscribeInput extends Component
 {
     public const IS_SUBSCRIBED_KEY = 'is_subscribed';
 
     /**
-     * @var bool
+     * @var bool|null
      */
-    public $subscribed;
+    public $subscribed = null;
 
-    public function __construct(private CheckoutSession $checkoutSession)
-    {
+    public function __construct(
+        private CheckoutSession $checkoutSession,
+        private Config $config
+    ) {
     }
 
     public function mount(): void
     {
-        $this->subscribed = $this->checkoutSession->getData(self::IS_SUBSCRIBED_KEY) ?: false;
+        $this->subscribed
+            = $this->checkoutSession->getData(self::IS_SUBSCRIBED_KEY) ?? $this->config->isCheckboxInitiallyEnabled();
     }
 
     public function updatedSubscribed(mixed $value) : mixed
